4e94ab1cbaa43c203176b6883b091aa437ff8cd9
teixeira83 <rafael.amtx@gmail.com>
2021-08-06 05:01:31 -0300

    Fix box-shadow of tbody

--- src/components/EmployedTable/index.js
+++ src/components/EmployedTable/index.js
@@ -33,11 +33,11 @@ const TableHead = styled.thead`
 
 const TableBody = styled.tbody`
   width: 100%;
+  background-color: #FFFFFF;
   
   & tr {
     height: 50px;
     box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.2);
-    background-color: #FFFFFF;
   }
   
   & > tr > td {
